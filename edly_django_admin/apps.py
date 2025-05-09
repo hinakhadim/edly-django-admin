820811b4d29fa6530dcdbe19001aac2351170d28
hinakhadim <hina.khadim@arbisoft.com>
2025-05-09 16:25:36 +0500

    fix: trying to add app_name at 0 index of installed_apps

--- edly_django_admin/apps.py
+++ edly_django_admin/apps.py
@@ -3,12 +3,14 @@ edly_django_admin Django application initialization.
 """
 
 from django.apps import AppConfig
+from django.conf import settings
 from edx_django_utils.plugins.constants import (
-     PluginURLs
+     PluginURLs, 
  )
  
 from openedx.core.djangoapps.plugins.constants import ProjectType
 
+app_label_edly = 'edly_django_admin.apps:EdlyDjangoAdminConfig'
 
 class EdlyDjangoAdminConfig(AppConfig):
     """
@@ -18,11 +20,28 @@ class EdlyDjangoAdminConfig(AppConfig):
     name = 'edly_django_admin'
 
     plugin_app = {
-         PluginURLs.CONFIG: {
+        PluginURLs.CONFIG: {
              ProjectType.LMS: {
-                 PluginURLs.NAMESPACE: 'user_custom',
-                 PluginURLs.REGEX: r'^api/user_custom/',
+                 PluginURLs.NAMESPACE: 'edly',
+                 PluginURLs.REGEX: r'^api/edly/',
                  PluginURLs.RELATIVE_PATH: 'urls',
             }
-        }
+        },
+        # 'settings_config': {
+        #     'lms.djangoapp': {
+        #         'production': { 'relative_path': 'settings.production' },
+        #     }
+        # },
     }
+
+    def ready(self):
+        self.prioritize_app()
+
+    def prioritize_app(self):
+        # 'edly_django_admin.apps.EdlyDjangoAdminConfig'
+        app_label = 'edly_django_admin.apps.EdlyDjangoAdminConfig'
+        # print("app-label -----> ", app_label, settings.INSTALLED_APPS)
+        if app_label in settings.INSTALLED_APPS:
+            # settings.INSTALLED_APPS.remove(app_label)
+            settings.INSTALLED_APPS.insert(0, app_label)
+            print("------Done----", settings.INSTALLED_APPS[:5])
--- edly_django_admin/urls.py
+++ edly_django_admin/urls.py
@@ -6,5 +6,5 @@ from django.views.generic import TemplateView  # pylint: disable=unused-import
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
-    # re_path(r'', TemplateView.as_view(template_name="edly_django_admin/base.html")),
+    re_path(r'hello', TemplateView.as_view(template_name="edly_django_admin/base.html")),
 ]
